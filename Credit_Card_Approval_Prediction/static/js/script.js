9d559cc7241cf21238ace279f4216b9446ec8391
Madhavi Latha <vyshupavuluri@gmail.com>
2026-07-14 20:59:53 +0530

    Delete Credit_Card_Approval_Prediction directory

--- Credit_Card_Approval_Prediction/static/js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("form")?.addEventListener("submit", () => {
-  const btn = document.querySelector("button");
-  if (btn) { btn.textContent = "Predicting..."; btn.disabled = true; }
-});
